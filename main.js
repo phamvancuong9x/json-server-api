8dbeffcc2c2d7bfde1122dfdc4e10d34203bb98b
unknown <anhcuonggt@gmail.com>
2021-12-02 11:13:25 +0700

    fix

--- main.js
+++ main.js
@@ -26,6 +26,6 @@ server.use((req, res, next) => {
 // Use default router
 server.use("/api", router);
 const PORT = process.env.PORT || 3000;
-server.listen(3000, () => {
+server.listen(PORT, () => {
   console.log("JSON Server is running");
 });
