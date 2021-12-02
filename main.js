28c33ad27e542e27fbe590aad3ec3661fc25ca1c
unknown <anhcuonggt@gmail.com>
2021-12-02 11:08:57 +0700

    fix

--- main.js
+++ main.js
@@ -1,30 +1,31 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
 
 // Set default middlewares (logger, static, cors and no-cache)
-server.use(middlewares)
+server.use(middlewares);
 
 // Add custom routes before JSON Server router
-server.get('/echo', (req, res) => {
-  res.jsonp(req.query)
-})
+server.get("/echo", (req, res) => {
+  res.jsonp(req.query);
+});
 
 // To handle POST, PUT and PATCH you need to use a body-parser
 // You can use the one used by JSON Server
-server.use(jsonServer.bodyParser)
+server.use(jsonServer.bodyParser);
 server.use((req, res, next) => {
-  if (req.method === 'POST') {
+  if (req.method === "POST") {
     // req.body.createdAt = Date.now()
     // req.body.updatedAt = Date.now()
   }
   // Continue to JSON Server router
-  next()
-})
+  next();
+});
 
 // Use default router
-server.use('/api',router)
+server.use("/api", router);
+const PORT = process.env.PORT || 3000;
 server.listen(3000, () => {
-  console.log('JSON Server is running')
-})
+  console.log("JSON Server is running");
+});
